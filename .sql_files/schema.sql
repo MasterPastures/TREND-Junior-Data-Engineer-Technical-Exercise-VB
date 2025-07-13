d99c57de45436fd8f021bafe479e2c8d8ef1b45c
MasterPastures <bahamvictor75@gmail.com>
2025-07-13 00:54:09 -0400

    Extracted SQL logic formerly in the `dataset_into_sql` function from analysis.py into this file instead.

---
+++ .sql_files/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS location (
+    id INTEGER PRIMARY KEY,
+    city text NOT NULL,
+    location_type text NOT NULL,
+    zipcode text NOT NULL,
+    borough text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS incident (
+    incident_id INTEGER PRIMARY KEY, 
+    agency text NOT NULL, 
+    complaint_type TEXT NOT NULL,
+    location_id INT NOT NULL,
+    descriptor text NOT NULL,
+    status text NOT NULL, 
+    created_date DATE NOT NULL, 
+    closed_date DATE NOT NULL, 
+    FOREIGN KEY (location_id) REFERENCES locations (id)
+);
