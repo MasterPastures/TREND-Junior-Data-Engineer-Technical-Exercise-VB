f5afafed965ee8b80ffce86e68e868b2e09246f7
MasterPastures <bahamvictor75@gmail.com>
2025-07-14 18:49:21 -0400

    Answered 2 out of 3 'interesting' questions about the data using SQL.

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -3,8 +3,7 @@ import argparse
 import gc
 from typing import Generator
 import sqlite3
-import datetime
-import numpy as np
+import os
 
 def read_large_file(file_path: str, limit: int, chunk_size: int) -> Generator:
     """
@@ -26,8 +25,17 @@ def read_large_file(file_path: str, limit: int, chunk_size: int) -> Generator:
     """
     path_with_limit =  file_path + '?$limit=' + str(limit)
     date_cols = ['created_date', 'closed_date']
-    for chunk in pd.read_csv(path_with_limit, chunksize=chunk_size, parse_dates=date_cols):
-        yield chunk
+    cols_needed = ['unique_key', 'agency', 'complaint_type',
+                        'descriptor', 'status', 'created_date', 
+                        'closed_date', 'location_type', 'city', 
+                        'incident_zip', 'borough']
+    dtype_dict = {'unique_key': str, 'agency': str, 'complaint_type': str,
+                  'descriptor': str, 'incident_status': str, 'location_type': str,
+                  'city': str, 'incident_zip': str, 'borough': str}
+    print("Starting CSV read...", flush=True)
+    for chunk in pd.read_csv(path_with_limit, chunksize=chunk_size, 
+                                          dtype=dtype_dict, usecols=cols_needed, parse_dates=date_cols):
+        yield chunk    
 
 def dataset_into_sql(dataset_generator: Generator):
     """
@@ -54,18 +62,16 @@ def dataset_into_sql(dataset_generator: Generator):
     for data_chunk in dataset_generator:
         incident_df = data_chunk[incident_columns]
         incident_df = incident_df.rename(columns={'unique_key': 'incident_id', 'status': 'incident_status'})
-        incident_df = incident_df.astype({'incident_id': str, 'agency': str, 'complaint_type': str,
-                                          'descriptor': str, 'incident_status': str, 'location_type': str})
         incident_df['location_id'] = data_chunk['incident_zip'].astype(str) + '_' + data_chunk['borough'].astype(str)
         incident_df.dropna(subset=['incident_id'], inplace=True)
+        incident_df.drop_duplicates(subset=['incident_id'], inplace=True)
 
 
         locations_df = data_chunk[locations_columns]
         locations_df = locations_df.rename(columns={'incident_zip': 'zipcode'})
-        locations_df = locations_df.astype({'city': str, 'zipcode': str,
-                                          'borough': str})
         locations_df['id'] = data_chunk['incident_zip'].astype(str) + '_' + data_chunk['borough'].astype(str)
-        locations_df.dropna(subset=['id'], inplace=True)
+        locations_df.dropna(subset=['zipcode', 'borough'], inplace=True)
+        locations_df.drop_duplicates(subset=['id'], inplace=True)
                 
         try:
             conn = sqlite3.connect('nyc311.db') # Connect to or create the database file
@@ -74,14 +80,15 @@ def dataset_into_sql(dataset_generator: Generator):
             cur = conn.cursor()
             cur.executescript(sql_script) # Execute the SQL script
             try:
-                incident_df.to_sql('incident', conn, if_exists='replace', index=False) # populate incident table
+                incident_df.to_sql('incident', conn, if_exists='append', index=False) # populate incident table
+                conn.commit()
             except sqlite3.IntegrityError as e:
                 print("Duplicate rows have been detected in the `incident` table.")
             try:
-                locations_df.to_sql('locations', conn, if_exists='replace', index=False) # populate locations table
+                locations_df.to_sql('locations', conn, if_exists='append', index=False) # populate locations table
+                conn.commit()
             except sqlite3.IntegrityError as e:
                 print("Duplicate rows have been detected in the `locations` table.")
-            print("Database and tables created successfully!")
             conn.commit()
             conn.close()
 
@@ -92,28 +99,69 @@ def dataset_into_sql(dataset_generator: Generator):
     
 
 def main():
-    gen = read_large_file('https://data.cityofnewyork.us/resource/erm2-nwe9.csv', 5, 5)
+
+    gen = read_large_file('https://data.cityofnewyork.us/resource/erm2-nwe9.csv', 50, 10)
     dataset_into_sql(gen)        
-
-    connection = sqlite3.connect("nyc311.db")
-
-    # cursor
-    crsr = connection.cursor()
-
-    # SQL command to create a table in the database
-    sql_command = """SELECT *
-    FROM locations;"""
-
-    # execute the statement
-    crsr.execute(sql_command)
-
-    ans = crsr.fetchall()
-
-    for i in ans:
-        print(i)    
-    # Answer 3 interesting questions about the dataset
     
+    connection = sqlite3.connect("nyc311.db")
+    
+    # Answer 3 interesting questions about the dataset
+
+    # [SQL] 1. We typically think of NYC as a single city -- how many distinct 'city' values are there?
+    # Could this column be misleading folks -- city employees and researchers alike -- when
+    # something like 'neighborhood' might be a better fit? Let's see based on how many values are
+    # returned.
+    
+    crsr_cities = connection.cursor()
+
+    how_many_cities = """SELECT 
+                        COUNT(DISTINCT city) AS unique_cities_in_nyc
+                        FROM locations;"""
+
+    crsr_cities.execute(how_many_cities)
+
+    ans_city = crsr_cities.fetchall()
+
+    output_cities = '../visualizations/unique_cities_in_nyc.txt'
+    with open(output_cities, 'w') as f:
+        # Write header (optional)
+        f.write(f"Number of unique cities: {ans_city}")
+        
+    print(f"Number of unique cities in NYC has been written to {output_cities}")  
+    
+    # [SQL] 2. Which borough typically has the quickest time of resolution for a ticket?
+    # I would assume this would be Manhattan since it's the most storied of the boroughs,
+    # and it's also the smallest. I would expect Queens and Brooklyn, two large boroughs
+    # that are less densely populated, to have less resources committed to resolving these issues. 
+
+    crsr_resolution = connection.cursor()
+
+    how_quick_resolution = """SELECT l.borough, AVG(JULIANDAY(i.closed_date) - JULIANDAY(i.created_date)) AS resolution_time_in_days
+                              FROM incident i
+                              JOIN locations l ON i.location_id = l.id
+                              WHERE i.closed_date IS NOT NULL AND i.created_date IS NOT NULL
+                              GROUP BY l.borough
+                              ORDER BY resolution_time_in_days DESC;"""
+
+    crsr_resolution.execute(how_quick_resolution)
+
+    ans_res = crsr_resolution.fetchall()
+
+    output_resolution = '../visualizations/resolution_time_by_borough.txt'
+    with open(output_resolution, 'w') as f:
+        # Write header (optional)
+        f.write(f"Number of unique cities: {ans_res}") 
+
+    print(f"Average time to resolution by borough has been written to {output_resolution}")  
+
+    # [Pandas] 3. What's the most common incident type in each borough?
+
+    common_incident_df = pd.read_sql_query("SELECT * from incident", connection)
+
+
+
     connection.close()
+    os.remove("nyc311.db")
 
     gc.collect()
 
