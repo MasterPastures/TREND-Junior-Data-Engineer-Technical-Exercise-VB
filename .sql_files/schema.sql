6eb4dfadf49e6cf281b373b870cb5b2bfceeb0bf
MasterPastures <bahamvictor75@gmail.com>
2025-07-13 22:56:10 -0400

    Changed data types in fields to better reflect the formatting of the ingested dataset.

--- .sql_files/schema.sql
+++ .sql_files/schema.sql
@@ -1,19 +1,20 @@
 CREATE TABLE IF NOT EXISTS locations (
-    id INTEGER PRIMARY KEY,
-    city text NOT NULL,
-    location_type text NOT NULL,
-    zipcode text NOT NULL,
-    borough text NOT NULL
+    id text PRIMARY KEY,
+    city text,
+    location_type text,
+    zipcode text,
+    borough text
 );
 
 CREATE TABLE IF NOT EXISTS incident (
-    incident_id INTEGER PRIMARY KEY, 
-    agency text NOT NULL, 
-    complaint_type TEXT NOT NULL,
-    location_id INT NOT NULL,
-    descriptor text NOT NULL,
-    incident_status text NOT NULL, 
-    created_date DATE NOT NULL, 
-    closed_date DATE NOT NULL, 
+    incident_id text PRIMARY KEY, 
+    agency text, 
+    complaint_type text,
+    location_type text,
+    location_id text,
+    descriptor text,
+    incident_status text, 
+    created_date DATE, 
+    closed_date DATE, 
     FOREIGN KEY (location_id) REFERENCES locations (id)
 );
